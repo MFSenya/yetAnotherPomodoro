b1d819636bceee4cf2835ca4aeeb93594591e73c
Senya <seni190999@mail.ru>
2026-07-20 16:54:05 +0500

    refactor(MainWindow): use QTabWidget as the central widget

--- src/views/main_window.py
+++ src/views/main_window.py
@@ -1,12 +1,16 @@
-from PySide6.QtWidgets import QMainWindow, QStackedWidget
+from PySide6.QtWidgets import QMainWindow, QTabWidget
 
 from .components.pomodorotimer.pomodorotimer_view import PomodoroTimerView
+from .tasklist_view import TaskListView
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("yetAnotherPomodoro")
-        self.stacked_widget = QStackedWidget(self)
-        self.pomodoro_timer = PomodoroTimerView()
-        self.stacked_widget.addWidget(self.pomodoro_timer)
-        self.setCentralWidget(self.stacked_widget)
+        self.tabs = QTabWidget(self)
+        self.setCentralWidget(self.tabs)
+        self.pomodoro_timer_view = PomodoroTimerView()
+        self.tasklist_view = TaskListView()
+        self.tabs.addTab(self.pomodoro_timer_view, "Timer")
+        self.tabs.addTab(self.tasklist_view, "Tasks")
+        self.tabs.setTabPosition(QTabWidget.West)
