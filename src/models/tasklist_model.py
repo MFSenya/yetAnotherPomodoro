25791e2234e0cc2b6c364f0604e3d867fab1fff9
Senya <seni190999@mail.ru>
2026-07-22 11:53:52 +0500

    feat(TaskListModel): use sqlite to store data

--- src/models/tasklist_model.py
+++ src/models/tasklist_model.py
@@ -1,49 +1,79 @@
-from dataclasses import dataclass, field, fields
 from datetime import timedelta, datetime
 from enum import StrEnum
-from typing import List, Optional
 
 from PySide6.QtCore import Qt, QAbstractTableModel, QModelIndex
+from sqlalchemy import create_engine, inspect, Interval, DateTime, String, Column, Integer
+from sqlalchemy.orm import declarative_base, sessionmaker
 
-@dataclass
-class Task:
+
+Base = declarative_base()
+
+class Task(Base):
+    __tablename__ = "tasks"
+
     class Status(StrEnum):
         OPEN = "Open"
         CLOSED = "Closed"
-        POSTPONED = "Postponed"
+    
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String, info={"alias": "Name"})
+    open_time = Column(DateTime, info={"alias": "Open time"})
+    time_spent = Column(Interval, info={"alias": "Time spent (H:M)"})
+    status = Column(String, info={"alias": "Status"})
 
-    name: str = field(metadata={"alias": "Name"})
-    open_time: datetime = field(metadata={"alias": "Open time"})
-    time_spent: timedelta = field(metadata={"alias": "Time spent (H:M)"})
-    status: Status = field(metadata={"alias": "Status"})
+    
+    def __init__(self, name: str, open_time: datetime, time_spent: timedelta, status: Status):
+        self.name = name
+        self.open_time = open_time
+        self.time_spent = time_spent
+        self.status = status
 
     @classmethod
     def get_field_meta(cls, field_name: str) -> dict:
         """Get metadata for a field."""
-        for f in fields(cls):
-            if f.name == field_name:
-                return f.metadata
+        column = cls.__table__.columns.get(field_name)
+        if column is not None:
+            return column.info
         raise AttributeError(f"Class {cls.__name__} doesn't have field with name {field_name}")
 
-class TaskListModel(QAbstractTableModel):
-    def __init__(self, tasks: Optional[List[Task]] = None):
-        super().__init__()
-        self._tasks = tasks or [Task("test", datetime(1999, 5, 12), timedelta(), Task.Status.OPEN)]
-        self._fields = fields(Task)
+    @classmethod
+    def get_headers(cls) -> list[str]:
+        headers = []
+        for column in cls.__table__.columns:
+            if column.name != 'id':
+                # Gat alias from info, if it is not there, use the columns with the name
+                headers.append(column.info.get("alias", column.name))
+        return headers
+        
 
-    def rowCount(self, parent=QModelIndex()):
+engine = create_engine('sqlite:///task_database.db', echo=False)
+Base.metadata.create_all(engine)
+SessionLocal = sessionmaker(bind=engine)
+
+class TaskListModel(QAbstractTableModel):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self._session = SessionLocal()
+        self._tasks : list[Task] = self._session.query(Task).all()
+        self._headers = Task.get_headers()
+        self._column_map = [column.key for column in inspect(Task).attrs if column.key != "id"]
+
+
+    def rowCount(self, parent=None):
         return len(self._tasks)
     
-    def columnCount(self, parent=QModelIndex()):
-        return len(self._fields)
+    def columnCount(self, parent=None):
+        return len(self._headers)
     
     def flags(self, index):
+        if not index.isValid():
+            return Qt.NoItemFlags
+        
         default_flags = super().flags(index)
 
-        field_info = self._fields[index.column()]
-
+        field_name = self._column_map[index.column()]
         # Only name and status columns are modifiable
-        if field_info.name in ("name", "status"):
+        if field_name in ("name", "status"):
             return default_flags | Qt.ItemFlag.ItemIsEditable
         
         return default_flags & ~Qt.ItemFlag.ItemIsEditable & ~Qt.ItemFlag.ItemIsSelectable
@@ -55,35 +85,32 @@ class TaskListModel(QAbstractTableModel):
             return None
         
         task = self._tasks[index.row()]
-        field_info = self._fields[index.column()]
-        value = getattr(task, field_info.name)
+        field_name = self._column_map[index.column()]
+        value = getattr(task, field_name)
 
         match role:
             case Qt.ItemDataRole.DisplayRole:
                  match value:
-                    case StrEnum():
+                    case Task.Status if field_name == "status":
                         return value.value
-                    case datetime():
+                    case datetime() if field_name == "open_time":
                         return value.strftime("%Y-%m-%d %H:%M")
-                    case timedelta():
+                    case timedelta() if field_name == "time_spent":
                         total_minutes = int(value.total_seconds() // 60)
                         hours, minutes = divmod(total_minutes, 60)
                         return f"{hours}:{minutes}"
                     case _:
                         return str(value)
             case Qt.ItemDataRole.EditRole:
-                return value
+                    return value
             case Qt.ItemDataRole.UserRole:
-                if field_info.name == "status":
+                if field_name == "status":
                     return [item.value for item in Task.Status]
-
-
         return None
     
     def headerData(self, section, orientation, role=Qt.ItemDataRole.DisplayRole):
         if orientation == Qt.Orientation.Horizontal and role == Qt.ItemDataRole.DisplayRole:
-            field_info = self._fields[section]
-            return field_info.metadata.get("alias", field_info.name)
+            return self._headers[section]
         return None
     
     def setData(self, index, value, role=Qt.ItemDataRole.EditRole):
@@ -91,26 +118,61 @@ class TaskListModel(QAbstractTableModel):
             return False
     
         task = self._tasks[index.row()]
-        field_info = self._fields[index.column()]
+        field_name = self._column_map[index.column()]
 
-        match field_info.name:
-            case "name":
-                task.name = str(value).strip()
-            case "status":
-                task.status = Task.Status(value)
-            case _:
-                return False
-            
-        self.dataChanged.emit(index, index, [Qt.ItemDataRole.DisplayRole])
+        try:
+            match field_name:
+                case "name":
+                    setattr(task, field_name, str(value).strip())
+                case "status":
+                    setattr(task, field_name, Task.Status(value))
+                case _:
+                    return False
 
-        return True    
+            self._session.commit()    
+            self.dataChanged.emit(index, index, [Qt.ItemDataRole.DisplayRole, Qt.ItemDataRole.EditRole])
+            return True
+        except Exception as e:
+            self._session.rollback()
+            print(f"An error occurred while saving the field {field_name}: {e}")
+        
+        return False
     
-    def add_task(self, task: Task):
-        self.beginInsertRows(QModelIndex(), self.rowCount(), self.rowCount())
-        self._tasks.append(task)
-        self.endInsertRows()
+    def add_task(self, name: str, open_time: datetime, time_spent: timedelta, status: Task.Status):
+        new_task = Task(name=name, open_time=open_time, time_spent=time_spent, status=status)
+        self._session.add(new_task)
+
+        try:
+            self._session.commit()
+            self._session.refresh(new_task)
+
+            row_count = len(self._tasks)
+            self.beginInsertRows(QModelIndex(), row_count, row_count)
+            self._tasks.append(new_task)
+            self.endInsertRows()
+            return True
+        except Exception as e:
+            self._session.rollback()
+            print(f"An error occured while adding a task: {e}")
+            return False
+
+
 
     def remove_task(self, row):
-        self.beginRemoveRows(QModelIndex(), row, row)
-        self._tasks.pop(row)
-        self.endRemoveRows()
+        if row < 0 or row >= len(self._tasks):
+            return False
+
+        task_to_delete = self._tasks[row]
+        self._session.delete(task_to_delete)
+
+        try:
+            self._session.commit()
+
+            self.beginRemoveRows(QModelIndex(), row, row)
+            self._tasks.pop(row)
+            self.endRemoveRows()
+            return True
+        except Exception as e:
+            self._session.rollback()
+            print(f"An error occured while removing a task: {e}")
+            return False
